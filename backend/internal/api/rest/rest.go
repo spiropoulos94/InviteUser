1d62166f9d98945ce1be5d34f466caf4b8f50c13
Nikos Spiropoulos <spiropoulos94@gmail.com>
2024-02-14 13:53:03 +0200

    cors fix

--- backend/internal/api/rest/rest.go
+++ backend/internal/api/rest/rest.go
@@ -29,8 +29,27 @@ func UserMiddleware() gin.HandlerFunc {
 	}
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "*")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func NewRouter() *gin.Engine {
-	r := gin.Default()
+	r := gin.New()
+
+	r.Use(CORSMiddleware())
 
 	r.Use(UserMiddleware())
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,26 @@ import { Box, Button } from "@mui/material";
 import Navbar from "./components/Navbar";
 import { useState } from "react";
 
+const getUsers = async () => {
+  try {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    headers.append("user-email", "usera@emailchaser.com");
+    headers.append("user-team", "Sales");
+
+    const response = await fetch("http://localhost:8080/api/users/", {
+      headers: headers,
+    }); // Adjusted endpoint
+    if (!response.ok) {
+      throw new Error("Failed to fetch users");
+    }
+    const users = await response.json();
+    console.log(users); // Do something with the users data, like updating state in React component
+  } catch (error) {
+    console.error("Error fetching users:", error);
+  }
+};
+
 function App() {
   const [user, setUser] = useState(null);
   const handleSignIn = () => {
@@ -9,6 +29,7 @@ function App() {
       name: "Nikos Spiropoulos",
       email: "test@emailchaser.com",
     });
+    getUsers();
   };
   return (
     <>
